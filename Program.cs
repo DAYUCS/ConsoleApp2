0f4f2b8ced1a5a3731dd8228eb7d798c107390ee
Dayu Bian <dayub@chinasystems.com>
2021-03-10 09:29:44 +0800

    Test Assembly.LoadFrom and Assembly.Load

--- Program.cs
+++ Program.cs
@@ -22,48 +22,98 @@ namespace ConsoleApp2
                                 }
                             }";
 
+        private static readonly string codeNew = @"using System;
+                            using ConsoleApp2;
+                            namespace Eximbills
+                            {
+                                public class Script1
+                                {
+                                    public static void Any(EximMarco eximMarco)
+                                    {
+                                        eximMarco.Decrease();
+                                    }
+                                }
+                            }";
+
         static void Main(string[] args)
         {
-            if (Type.GetType("Eximbills.Script1, EximScript1, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null") == null) {
-                Console.WriteLine("Compile Script into dll...");
-                var tree = SyntaxFactory.ParseSyntaxTree(code);
-                var compilation = CSharpCompilation.Create(
-                    "EximScript1",
-                    options: new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary),
-                    syntaxTrees: new[] { tree },
-                    references: new[] { MetadataReference.CreateFromFile(typeof(object).Assembly.Location),
-                                        MetadataReference.CreateFromFile(typeof(ExpandoObject).Assembly.Location),
-                                        MetadataReference.CreateFromFile(typeof(Microsoft.CSharp.RuntimeBinder.Binder).Assembly.Location),
-                                        MetadataReference.CreateFromFile(typeof(EximMarco).Assembly.Location)});
+            Assembly compiledAssembly;
 
-                //Generate dll and pdb
-                var emitResult = compilation.Emit("dlls\\EximScript1.dll");
-                Console.WriteLine("Compiled.");
-
-                //Generate Assembly and get type's Assembly Qualified Name
-                using (var stream = new MemoryStream())
-                {
-                    var compileResult = compilation.Emit(stream);
-                    Assembly compiledAssembly = Assembly.Load(stream.GetBuffer());
-                    Type calculator = compiledAssembly.GetType("Eximbills.Script1");
-                    Console.WriteLine($"Assembly Qualified Name: {calculator.AssemblyQualifiedName}");
-                }
+            if (Type.GetType("Eximbills.Script1, EximScript1, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null") == null)
+            {
+                CompileProgram(code, true);
             }
 
+            // Type.GetType will load assembly from dll e.g. Assembly.LoadFrom(dll file)
             for (int i = 0; i < 10; i++) {
                 Type calculator = Type.GetType("Eximbills.Script1, EximScript1, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null");
-                if (calculator == null)
-                {
-                    Console.WriteLine("Load assembly from dll...");
-                    Assembly compiledAssembly = Assembly.LoadFrom("dlls\\EximScript1.dll");
-                    calculator = compiledAssembly.GetType("Eximbills.Script1");
-                }
                 object instance = Activator.CreateInstance(calculator);
                 EximMarco eximMarco = new EximMarco(i);
                 calculator.InvokeMember("Any", BindingFlags.InvokeMethod, null, instance, new object[] { eximMarco });
                 Console.WriteLine("Step:{0} -- Result: {1}", i, eximMarco.Count); 
             }
+
+            // Load assembly with Assembly.Load(byte[]), this will cause memory leak if loading again and again!
+            compiledAssembly = CompileProgram(codeNew, false);
+            for (int i = 0; i < 10; i++)
+            {
+                Type calculator = compiledAssembly.GetType("Eximbills.Script1");
+                object instance = Activator.CreateInstance(calculator);
+                EximMarco eximMarco = new EximMarco(i);
+                calculator.InvokeMember("Any", BindingFlags.InvokeMethod, null, instance, new object[] { eximMarco });
+                Console.WriteLine("Step:{0} -- Result: {1}", i, eximMarco.Count);
+            }
+
+            //Get all assemblies loaded, there will be two EximScript1!
+            Assembly[] assemblies = AppDomain.CurrentDomain.GetAssemblies();
             Console.ReadKey();
+        }
+
+        private static Assembly CompileProgram(string code, bool usingDll)
+        {
+            Console.WriteLine("Compile Script...");
+            Assembly compiledAssembly;
+            var tree = SyntaxFactory.ParseSyntaxTree(code);
+            var compilation = CSharpCompilation.Create(
+                "EximScript1",
+                options: new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary),
+                syntaxTrees: new[] { tree },
+                references: new[] { MetadataReference.CreateFromFile(typeof(object).Assembly.Location),
+                                        MetadataReference.CreateFromFile(typeof(ExpandoObject).Assembly.Location),
+                                        MetadataReference.CreateFromFile(typeof(Microsoft.CSharp.RuntimeBinder.Binder).Assembly.Location),
+                                        MetadataReference.CreateFromFile(typeof(EximMarco).Assembly.Location)});
+
+            //Generate dll and pdb
+            if (usingDll)
+            {
+                compilation.Emit("dlls\\EximScript1.dll");
+            }
+            
+            Console.WriteLine("Compiled.");
+
+            if (usingDll) {
+                compiledAssembly = Assembly.LoadFrom("dlls\\EximScript1.dll");
+                /* Test Assembly.LoadFrom()
+                for (int i = 0; i < 10; i++)
+                {
+                    Assembly.LoadFrom("dlls\\EximScript1.dll");
+                }
+                */
+            } else {
+                using (var stream = new MemoryStream())
+                {
+                    compilation.Emit(stream);
+                    /* Test Assembly.Load()
+                    for (int i =0; i < 10; i++)
+                    {
+                        Assembly.Load(stream.GetBuffer());
+                    }
+                    */
+                    compiledAssembly = Assembly.Load(stream.GetBuffer());
+                }
+            }
+
+            return compiledAssembly;
         }
     }
 
@@ -83,6 +133,11 @@ namespace ConsoleApp2
         public void Increase()
         {
             Count++;
+        }
+
+        public void Decrease()
+        {
+            Count--;
         }
     }
 }
